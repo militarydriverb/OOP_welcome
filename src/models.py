e008ef39907caf46883ce248e6ac62f33f1d45af
Eugene Zorin <militarydriverb@gmail.com>
2025-10-11 20:46:50 +0300

    homework_17_1 init

--- src/models.py
+++ src/models.py
@@ -203,54 +203,24 @@ class Category(BaseCategory):
         return self.__products
 
 
-if __name__ == "__main__":  # pragma: no cover
-    product1 = Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )  # pragma: no cover
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)  # pragma: no cover
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)  # pragma: no cover
+if __name__ == '__main__':
+    try:
+        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0) # pragma: no cover
+    except ValueError as e: # pragma: no cover
+        print(
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством"
+        ) # pragma: no cover
+    else:
+        print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством") # pragma: no cover
 
-    print(product1.name)  # pragma: no cover
-    print(product1.description)  # pragma: no cover
-    print(product1.price)  # pragma: no cover
-    print(product1.quantity)  # pragma: no cover
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5) # pragma: no cover
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8) # pragma: no cover
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14) # pragma: no cover
 
-    print(product2.name)  # pragma: no cover
-    print(product2.description)  # pragma: no cover
-    print(product2.price)  # pragma: no cover
-    print(product2.quantity)  # pragma: no cover
+    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3]) # pragma: no cover
 
-    print(product3.name)  # pragma: no cover
-    print(product3.description)  # pragma: no cover
-    print(product3.price)  # pragma: no cover
-    print(product3.quantity)  # pragma: no cover
+    print(category1.middle_price()) # pragma: no cover
 
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, "
-        "но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3],
-    )  # pragma: no cover
-
-    print(category1.name == "Смартфоны")  # pragma: no cover
-    print(category1.description)  # pragma: no cover
-    print(len(category1.products_in_list))  # pragma: no cover
-    print(category1.category_count)  # pragma: no cover
-    print(category1.product_count)  # pragma: no cover
-
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)  # pragma: no cover
-    category2 = Category(
-        "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, "
-        "станет вашим другом и помощником",
-        [product4],
-    )  # pragma: no cover
-
-    print(category2.name)  # pragma: no cover
-    print(category2.description)  # pragma: no cover
-    print(len(category2.products_in_list))  # pragma: no cover
-    print(category2.products)  # pragma: no cover
-
-    print(Category.category_count)  # pragma: no cover
-    print(Category.product_count)  # pragma: no cover
+    category_empty = Category("Пустая категория", "Категория без продуктов", []) # pragma: no cover
+    print(category_empty.middle_price()) # pragma: no cover
 
